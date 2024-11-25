493caf38d21ca4166bda84720cd90cb21febdfe8
tbassis <tulioassis.18@gmail.com>
2024-11-25 12:30:14 -0300

    Created a func to get a entire collection from database to reuse

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -5,23 +5,20 @@ import conectarAoBanco from "../config/dbconfig.js";
 // stores all data related to the database cluster connection
 const connection = await conectarAoBanco(process.env.CONNECTIONDB_STRING)
 
-// connect to bookstoreDB, get the entire collection of books and return to the controller
-export async function getAllBooks() {
-	// connect to the specific database  
-    const db = connection.db("bookstore-node-api");
+function getCollection(collectionName) {
+    // connect to the specific database  
+    const db = connection.db(process.env.MONGODB_NAME);
 
-	// stores a collection from the Database
-    const collection = db.collection("books");
+	// stores a collection from the database
+    const collection = db.collection(collectionName);
 
-	// filter — The filter predicate. If unspecified, then all documents in the collection will match the predicate
     return collection.find().toArray();
 }
 
-// connect to bookstoreDB, get the entire collection of authors and return to the controller
+export async function getAllBooks() {
+    return getCollection("books")
+}
+
 export async function getAllAuthors() {
-    const db = connection.db("bookstore-node-api");
-
-    const collection = db.collection("authors");
-
-    return collection.find().toArray();
+    return getCollection("authors");
 }
