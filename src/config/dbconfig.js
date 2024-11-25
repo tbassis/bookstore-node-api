2613fce860d7aa592445e4ca448ef2037837a8ad
tbassis <tulioassis.18@gmail.com>
2024-11-25 14:16:34 -0300

    Add comments with details of what its being done

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,11 +1,20 @@
+// dbconfig.js - Creates a MongoDB Client and stablish connect to the the MongoDB Atlas with a connection string
+// https://www.mongodb.com/products/platform/atlas-database
+
 import { MongoClient } from "mongodb";
 
+// connectToDatabase - will stablish the connection with the MongoDB using the given database connection string
 export default async function connectToDatabase(conectionString) {
+
+  // this object will represent the connection with the database
   let mongoClient;
 
   try {
+      // create a new instance of MongoClient using the necessary information thats written in the connection string
       mongoClient = new MongoClient(conectionString);
       console.log("Connection to database cluster...");
+
+      // with a instanced MongoClient it will connect with the Mongo
       await mongoClient.connect();
       console.log("Successfully connected to MongoDB Atlas!");
 
