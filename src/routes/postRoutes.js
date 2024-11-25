f079b45993e6f067f8fd7f03c6af8d1f668a3422
tbassis <tulioassis.18@gmail.com>
2024-11-25 11:12:42 -0300

    Index the books route to return all books in the database

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,29 +1,12 @@
 import express from "express";
-import conectarAoBanco from "../config/dbconfig.js";
-
-// stores all data related to the database cluster connection
-const connection = await conectarAoBanco(process.env.CONNECTIONDB_STRING)
-
-async function getAllBooks() {
-	// connect to the specific database  
-    const db = connection.db("bookstore-node-api");
-
-	// stores a collection from the Database
-    const collection = db.collection("books");
-
-	// filter — The filter predicate. If unspecified, then all documents in the collection will match the predicate
-    return collection.find().toArray();
-}
+import listAllBooks from "../controllers/postController.js";
 
 const routes = (app) => {
     // defines to the server to return a json object to the client 
     app.use(express.json())
 
      // index the function that will be running when the server call that route
-     app.get("/books", async (rec, res) => {
-        const allPosts = await getAllBooks();
-        res.status(200).json(allPosts); 
-    });
+     app.get("/books", listAllBooks);
 }
 
 export default routes;
