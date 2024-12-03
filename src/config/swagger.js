afaa8eea7f7b3e0864606e015d141a4910ffdb32
tbassis <tulioassis.18@gmail.com>
2024-12-03 16:18:35 -0300

    Add Swagger config file to the project

---
+++ src/config/swagger.js
@@ -0,0 +1,42 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"
+
+const options = {
+
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Bookstore Node API',
+      description: "Este repositório tem como finalidade servir de caso de estudo de desenvolvimento de uma api utlizando o framework Express de Node.js",
+      contact: {
+        name: "Túlio Assis",
+        email: "tulioassis.18@gmail.com",
+        url: "https://github.com/tbassis/bookstore-node-api"
+      },
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: "http://localhost:8080/",
+        description: "Local server"
+      },
+    ]
+  },
+  // looks for configuration in specified directories
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(options)
+
+function swaggerDocs(app, port) {
+  // Swagger Page
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+  // Documentation in JSON format
+  app.get("/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json")
+    res.send(swaggerSpec)
+  })
+};
+
+export default swaggerDocs
