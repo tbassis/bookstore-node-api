f19ae786b8336c411ace6e34583753962adfbad5
tbassis <tulioassis.18@gmail.com>
2024-12-03 15:22:00 -0300

    Add multer endpoint to upload image

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,16 +1,23 @@
 import express from "express";
-import { listAllBooks, listAllAuthors, createNewBook, createNewAuthor } from "../controllers/postController.js";
+import multer from "multer";
+import { listAllBooks, listAllAuthors, createNewBook, createNewAuthor, insertNewEvaluation } from "../controllers/postController.js";
+
+// define the destination of the files that are uploaded to the server
+const upload = multer({dest: "./uploads"})
 
 const routes = (app) => {
     // defines to the server to return a json object to the client 
     app.use(express.json())
 
-     // index the function that will be running when the server call that route
-     app.get("/books", listAllBooks);
-     app.post("/books", createNewBook)
+    // index the function that will be running when the server call that route
+    app.get("/books", listAllBooks);
+    app.post("/books", createNewBook)
 
-     app.get("/authors", listAllAuthors);
-     app.post("/author", createNewAuthor);
+    app.get("/authors", listAllAuthors);
+    app.post("/author", createNewAuthor);
+
+    // upload.single("image") define that the key of the form-data to upload the image is image
+    app.post("/evaluation", upload.single("image"), insertNewEvaluation);
 }
 
 export default routes;
